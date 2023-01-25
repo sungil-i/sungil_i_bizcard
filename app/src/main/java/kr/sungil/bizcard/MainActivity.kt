2d797b3d04b50f77a50b3deb5bb3701acde1e24f
SiYoung0 <122767323+SiYoung0@users.noreply.github.com>
2023-01-25 14:39:14 +0900

    나의 BizCard 완성

--- app/src/main/java/kr/sungil/bizcard/MainActivity.kt
+++ app/src/main/java/kr/sungil/bizcard/MainActivity.kt
@@ -8,6 +8,7 @@ import kr.sungil.bizcard.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
 	private lateinit var adapter: PortfolioAdapter
+	private var isShow: Boolean = false
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
 		setContentView(binding.root)
 
 		// Code Here
+		// 리싸이클러뷰 Adapter 연결
+		binding.rvPort.adapter = adapter
+		// 리싸이클려뷰 데이터리스트 연결
+		adapter.submitList(Portfolios.items)
 
+		binding.btToggle.setOnClickListener {
+			isShow = !isShow
+			binding.rvPort.isVisible = isShow
+		}
 	}
 }
